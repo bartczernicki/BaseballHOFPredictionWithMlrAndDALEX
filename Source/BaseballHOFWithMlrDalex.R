edc521df9bfd64187a32285603f760242a20abb8
Bart Czernicki <39144110+bartczernicki@users.noreply.github.com>
2018-07-26 16:18:38 -0400

    Updates to the code

--- Source/BaseballHOFWithMlrDalex.R
+++ Source/BaseballHOFWithMlrDalex.R
@@ -1,8 +1,12 @@
 # TESTED ON MRO 3.5.0 & CRAN R 3.5.1
+# Check your R environment version
+version
+
+
 
 # 1) INSTALL & LOAD PACKAGES
-
 # a) INSTALL Core Modeling Packages
+if('ceterisParibus' %in% rownames(installed.packages()) == FALSE) {install.packages('ceterisParibus')}
 if('kernlab' %in% rownames(installed.packages()) == FALSE) {install.packages('kernlab')}
 if('randomForest' %in% rownames(installed.packages()) == FALSE) {install.packages('randomForest')}
 if('xgboost' %in% rownames(installed.packages()) == FALSE) {install.packages('xgboost')}
@@ -12,19 +16,22 @@ if('mlr' %in% rownames(installed.packages()) == FALSE) {install.packages('mlr')}
 
 # b) LOAD Package
 # Core Modeling Packages
+library(ceterisParibus)     # Used for What-If Charts
 library(kernlab)
-library(randomForest)
-library(xgboost)
+library(randomForest)       # Used For RandomForest classifier
+library(xgboost)            # Used for XgBoost classifier
 # Core Modeling Packages
-library(DALEX)
-library(mlr)
+library(DALEX)              # Used for Model Explainers
+library(mlr)                # Used for training/orchestrating models
+
 
 
 # 2) SETUP TRAINING, VALIDATION & COMBINED DATA SETS
 
 # a) Set working directory
 getwd()
-wdPath <- "C:\\Users\\bart\\Downloads\\BaseballHOFPredictionWithMlrAndDALEX-master\\BaseballHOFPredictionWithMlrAndDALEX-master\\Source"
+wdPath <- "/Users/bartczernicki-msft/Desktop/SourceCode/BaseballHOFDalex"
+# wdPath <- "C:\\Users\\bart\\Downloads\\BaseballHOFPredictionWithMlrAndDALEX-master\\BaseballHOFPredictionWithMlrAndDALEX-master\\Source"
 setwd(wdPath)
 
 # b) Load Training Data CSV Files
@@ -49,12 +56,10 @@ nrow(trainingData)
 nrow(validationData)
 nrow(combinedData)
 
-# f) Change InductedToHallOfFame to a factor for model training
-# trainingData$InductedToHallOfFame <- factor(trainingData$InductedToHallOfFame)
-# validationData$InductedToHallOfFame <- factor(validationData$InductedToHallOfFame)
-
-# g) Check Validation Data
+# f) Check Validation Data
 head(validationData, 15)
+
+
 
 # 3) MACHINE LEARNING - TRAINING
 
@@ -66,20 +71,20 @@ classif_task <- makeClassifTask(id = "class1", data = trainingData, target = "In
 classif_task_combined <- makeClassifTask(id = "class2", data = combinedData, target = "InductedToHallOfFame", positive = 1)
 
 # C) Make Learners - Random Forest, GLM, XgBoost (MLR package construct)
+# Get a list of available learners (will list available and what learners are installed)
+listLearners("classif", properties = c("twoclass", "prob"))
+
+# Make different learners (simple - using default hyperparameters)
 classif_lrn_rf <- makeLearner("classif.randomForest", predict.type = "prob")
 classif_lrn_glm <- makeLearner("classif.binomial", predict.type = "prob")
 classif_lrn_xgboost <- makeLearner("classif.xgboost", predict.type = "prob")
-xgboostParams <- list(nrounds = 400, max_depth = 30, nthread = 4, max_delta_step = 6,
-                      num_parallel_tree = 4, eta=0.02, gamma = 1)
-classif_lrn_xgboost <- makeLearner("classif.xgboost", predict.type = "prob", par.vals = xgboostParams)
 
 # d) Traing the classification models using learners (and their accompanying parameters)
-?configureMlr
-getMlrOptions()
+# Run this command for MLR configuration: ?configureMlr
+# Get MLR options getMlrOptions()
 classif_rf <- train(classif_lrn_rf, classif_task)
 classif_glm <- train(classif_lrn_glm, classif_task)
 classif_xgboost <- train(classif_lrn_xgboost, classif_task)
-classif_xgboostTwo <- train(classif_lrn_xgboost, classif_task_combined)
 
 # e) Make predictions on validation data
 predRf = predict(classif_rf, newdata = validationData)
@@ -87,12 +92,16 @@ predGlm = predict(classif_glm, newdata = validationData)
 predXgBoost = predict(classif_xgboost, newdata = validationData)
 predXgBoostTwo = predict(classif_xgboostTwo, newdata = validationData)
 
+
+
 # 4) MACHINE LEARNING - EVALUATION
 
 # a) Test Performance of classifiers (models) using various performance metrics
+
 # You can get a list of available measures
 listMeasures("classif")
 getDefaultMeasure(classif_lrn_rf)
+# View performance metrics
 performanceMetrics <- list(tnr, tpr, lsr, f1, mmce, tp, acc, fdr, kappa, ppv)
 performance(predRf, measures = performanceMetrics)
 performance(predGlm, measures = performanceMetrics)
@@ -118,9 +127,9 @@ rocMeasures_glm
 rocMeasures_XgBoost <- calculateROCMeasures(predXgBoost)
 rocMeasures_XgBoost
 
-# Plot ROC Curves
+# Plot ROC Curves for the trained classifiers/models
 rocCurvesData <- generateThreshVsPerfData(list(rf = predRf, glm = predGlm, xgboost = predXgBoost), 
-                              measures = list(fpr, tpr)) 
+                                          measures = list(fpr, tpr)) 
 plotROCCurves(rocCurvesData)
 
 # d) Plot Learner Prediction - All-Star Appearances vs 
@@ -144,31 +153,6 @@ plotLearnerPrediction(classif_lrn_xgboost, task = classif_task, features = c("H"
 plotLearnerPrediction(classif_lrn_rf, task = classif_task, features = c("MVPs", "H"), cv = 10)
 
 
-
-# Hyper Parameters
-getParamSet("classif.xgboost")
-getParamSet("classif.randomForest")
-discrete_ps <- makeParamSet(
-  makeDiscreteParam("ntree", values = c(1, 50, 100, 150, 400, 600)),
-  makeLogicalParam("replace")
-)
-tuneControl <- makeTuneControlRandom(maxit = 10L)
-rdesc = makeResampleDesc("CV", iters = 3L)
-resampling <- rdesc
-
-resultsTuning <- tuneParams("classif.randomForest", task = classif_task, resampling = rdesc,
-                            par.set = discrete_ps, 
-                            measures = list(ppv, setAggregation(ppv, test.sd)),
-                            control = tuneControl)
-
-# Custom Holdout
-rin = makeFixedHoldoutInstance(train.inds = 1:6258, test.inds = 6259:9385, size=9385)
-rin
-resultsTuning <- tuneParams(#"classif.randomForest"
-                            classif_lrn_rf, task = classif_task_combined, resampling = rdesc,
-                            par.set = discrete_ps, 
-                            measures = list(auc, setAggregation(auc, test.sd)),
-                            control = tuneControl)
 
 #4) MACHINE LEARNING - MODEL EXPLAINERS
 # Uses the DALEX r package
@@ -203,68 +187,53 @@ plot(vi_classif_xgboost)
 # Variable Response - All Star Appearances Feature
 vr_AllStarAppearances_rf  <- variable_response(explainer_classif_rf, variable = "AllStarAppearances", type = "pdp")
 vr_AllStarAppearances_glm  <- variable_response(explainer_classif_glm, variable = "AllStarAppearances", type = "pdp")
-vr_AllStarAppearances_svm  <- variable_response(explainer_classif_svm, variable = "AllStarAppearances", type = "pdp")
 vr_AllStarAppearances_xgboost  <- variable_response(explainer_classif_xgboost, variable = "AllStarAppearances", type = "pdp")
-plot(vr_AllStarAppearances_rf, vr_AllStarAppearances_glm, vr_AllStarAppearances_svm, vr_AllStarAppearances_xgboost)
+plot(vr_AllStarAppearances_rf, vr_AllStarAppearances_glm, vr_AllStarAppearances_xgboost)
 
 # Variable Response - Random Forest Model - HR Feature
 vr_hr_rf  <- variable_response(explainer_classif_rf, variable = "HR", type = "pdp")
 vr_hr_glm  <- variable_response(explainer_classif_glm, variable = "HR", type = "pdp")
 vr_hr_xgboost  <- variable_response(explainer_classif_xgboost, variable = "HR", type = "pdp")
 plot(vr_hr_rf, vr_hr_glm, vr_hr_xgboost)
-# Variable Response - Random Forest Model - MVP Feature
-pdp_classif_rf  <- variable_response(explainer_classif_rf, variable = "MVPs", type = "pdp")
-pdp_classif_glm  <- variable_response(explainer_classif_glm, variable = "MVPs", type = "pdp")
-pdp_classif_svm  <- variable_response(explainer_classif_svm, variable = "MVPs", type = "pdp")
-plot(pdp_classif_rf, pdp_classif_glm, pdp_classif_svm)
-# Variable Response - Random Forest Model - TB (Total Bases) Feature
-pdp_classif_rf  <- variable_response(explainer_classif_rf, variable = "TB", type = "pdp")
-pdp_classif_glm  <- variable_response(explainer_classif_glm, variable = "TB", type = "pdp")
-pdp_classif_svm  <- variable_response(explainer_classif_svm, variable = "TB", type = "pdp")
-plot(pdp_classif_rf, pdp_classif_glm, pdp_classif_svm)
-# Variable Response - Random Forest Model - GoldGloves Feature
-pdp_classif_rf  <- variable_response(explainer_classif_rf, variable = "GoldGloves", type = "pdp")
-pdp_classif_glm  <- variable_response(explainer_classif_glm, variable = "GoldGloves", type = "pdp")
-pdp_classif_svm  <- variable_response(explainer_classif_svm, variable = "GoldGloves", type = "pdp")
-plot(pdp_classif_rf, pdp_classif_glm, pdp_classif_svm)
 
+# Variable Response - Random Forest Model - MVP Feature
+vr_mvp_rf  <- variable_response(explainer_classif_rf, variable = "MVPs", type = "pdp")
+vr_mvp_glm  <- variable_response(explainer_classif_glm, variable = "MVPs", type = "pdp")
+vr_mvp_xgboost  <- variable_response(explainer_classif_xgboost, variable = "MVPs", type = "pdp")
+plot(vr_mvp_rf, vr_mvp_glm, vr_mvp_xgboost)
+
+# Variable Response - Random Forest Model - TB (Total Bases) Feature
+vr_tb_rf  <- variable_response(explainer_classif_rf, variable = "TB", type = "pdp")
+vr_tb_glm  <- variable_response(explainer_classif_glm, variable = "TB", type = "pdp")
+vr_tb_xgboost  <- variable_response(explainer_classif_xgboost, variable = "TB", type = "pdp")
+plot(vr_tb_rf, vr_tb_glm, vr_tb_xgboost)
+
+# Variable Response - Random Forest Model - GoldGloves Feature
+vr_goldGloves_rf  <- variable_response(explainer_classif_rf, variable = "GoldGloves", type = "pdp")
+vr_goldGloves_glm  <- variable_response(explainer_classif_glm, variable = "GoldGloves", type = "pdp")
+vr_goldGloves_xgboost  <- variable_response(explainer_classif_xgboost, variable = "GoldGloves", type = "pdp")
+plot(vr_goldGloves_rf, vr_goldGloves_glm, vr_goldGloves_xgboost)
+
+#5) MACHINE LEARNING - INDIVIDUAL PREDICTIONS
 
 # Prediction Breakdown - Ichiro Suzuki
-newPred31 <- validationData[31,]
-newPredDownRf31 <- prediction_breakdown(explainer_classif_rf, observation = newPred31)
-newPredDownRf31
-plot(newPredDownRf31)
-
-# Prediction Breakdown - Kirby Puckett
-newPred31 <- validationData[22,]
-newPredDownRf31 <- prediction_breakdown(explainer_classif_rf, observation = newPred31)
-newPredDownRf31
-plot(newPredDownRf31)
-
-
-# Prediction Breakdown - Willie Mays
-newPred12 <- validationData[12,]
-newPredDownRf12 <- prediction_breakdown(explainer_classif_rf, observation = newPred12)
-newPredDownRf12
-plot(newPredDownRf12)
-
-# Prediction Breakdown - Derek Jeter
-newPred29 <- validationData[29,]
-newPredDownRf29 <- prediction_breakdown(explainer_classif_rf, observation = newPred29)
-newPredDownRf29
-plot(newPredDownRf29)
-
-# Prediction Breakdown - Tony Perez
-newPred18 <- validationData[18,]
-newPredDownRf18 <- prediction_breakdown(explainer_classif_rf, observation = newPred18)
-newPredDownRf18
-plot(newPredDownRf18)
-
+ichiroSuzukiData = head(fullPlayerData[fullPlayerData$FullPlayerName == "Ichiro Suzuki",], 1)
+prediction_breakdown_IchiroSuzuki <- prediction_breakdown(explainer_classif_rf, observation = ichiroSuzukiData)
+prediction_breakdown_IchiroSuzuki
+plot(prediction_breakdown_IchiroSuzuki)
+# What-If - Ichiro Suzuki
+whatIf_rf_ichiroSuzuki <- ceteris_paribus(explainer_classif_rf, observation = ichiroSuzukiData, selected_variables = c("MVPs"))
+whatIf_rf_ichiroSuzuki
+plot(whatIf_rf_ichiroSuzuki, split = "variables", color = "variables")
 
 # Prediction Breakdown - Kirby Puckett
 kirbyPuckettData = head(fullPlayerData[fullPlayerData$FullPlayerName == "Kirby Puckett",], 1)
 prediction_breakdown_KirbyPuckett = prediction_breakdown(explainer_classif_rf, observation = kirbyPuckettData)
 plot(prediction_breakdown_KirbyPuckett)
+# What-If - Kirby Puckett
+whatIf_rf_kirbyPuckett <- ceteris_paribus(explainer_classif_rf, observation = kirbyPuckettData)
+whatIf_rf_kirbyPuckett
+plot(whatIf_rf_kirbyPuckett)
 
 # Prediction Breakdown - Willie Mays
 willieMaysData = head(fullPlayerData[fullPlayerData$FullPlayerName == "Willie Mays",], 1)
@@ -281,15 +250,52 @@ chaseUtleyData = head(fullPlayerData[fullPlayerData$FullPlayerName == "Chase Utl
 prediction_breakdown_ChaseUtley = prediction_breakdown(explainer_classif_rf, observation = chaseUtleyData)
 plot(prediction_breakdown_ChaseUtley)
 
+# Prediction Breakdown - Mike Trout
+mikeTroutData = head(fullPlayerData[fullPlayerData$FullPlayerName == "Mike Trout",], 1)
+prediction_breakdown_mikeTroutData = prediction_breakdown(explainer_classif_rf, observation = mikeTroutData)
+plot(prediction_breakdown_mikeTroutData)
+# What-If - Mike Trout
+whatIf_rf_mikeTrout <- ceteris_paribus(explainer_classif_rf, observation = mikeTroutData, grid_points = 1000)
+whatIf_rf_mikeTrout
+plot(whatIf_rf_mikeTrout)
+
 # Prediction Breakdown - Jimmy Rollins
 jimmyRollinsData = head(fullPlayerData[fullPlayerData$FullPlayerName == "Jimmy Rollins",], 1)
 prediction_breakdown_JimmyRollins = prediction_breakdown(explainer_classif_rf, observation = jimmyRollinsData)
 plot(prediction_breakdown_JimmyRollins)
+# What-If - Jimmy Rollins
+whatIf_rf_jimmyRollins <- ceteris_paribus(explainer_classif_rf, observation = jimmyRollinsData)
+whatIf_glm_jimmyRollins <- ceteris_paribus(explainer_classif_glm, observation = jimmyRollinsData)
+jimmyRollinsDataXgBoost <- subset(jimmyRollinsData, select = -c(FullPlayerName, LastYearPlayed, ID))
+whatIf_xgBoost_jimmyRollins <- ceteris_paribus(explainer_classif_xgboost, observation = jimmyRollinsDataXgBoost)
+plot(whatIf_xgBoost_jimmyRollins)
 
 # Prediction Breakdown - Jeff Kent
 jeffKentData = head(fullPlayerData[fullPlayerData$FullPlayerName == "Jeff Kent",], 1)
 prediction_breakdown_JeffKent = prediction_breakdown(explainer_classif_rf, observation = jeffKentData)
 plot(prediction_breakdown_JeffKent)
+# What-If - Jeff Kent
+whatIf_rf_jeffKent <- ceteris_paribus(explainer_classif_rf, observation = jeffKentData)
+plot(whatIf_rf_jeffKent)
+selectedVariables <- c('H')
+whatIf_rf_jeffKentAllStarApperances <- ceteris_paribus(explainer_classif_rf,
+  observation = jeffKentData, selected_variables = selectedVariables, grid_points = 4)
+plot(whatIf_rf_jeffKentAllStarApperances)
+str(whatIf_rf_jeffKentAllStarApperances)
+summary(whatIf_rf_jeffKentAllStarApperances)
+whatIf_rf_jeffKentAllStarApperances
+
+whatIf_rf_jeffKentAllStarApperances
+
+#Modified Prediction
+jeffKentData = head(fullPlayerData[fullPlayerData$FullPlayerName == "Jeff Kent",], 1)
+jeffKentData$H <- jeffKentData$H * 1.0092710997
+prediction_breakdown_JeffKent = prediction_breakdown(explainer_classif_rf, observation = jeffKentData)
+plot(prediction_breakdown_JeffKent)
+attributes(prediction_breakdown_JeffKent)
+
+
+# Selected 2nd basemen
 
 # Prediction Breakdown - Rod Carew
 rodCarewData = head(fullPlayerData[fullPlayerData$FullPlayerName == "Rod Carew",], 1)
